906c7be06c1201e7dd2ed40919962ad7b787c231
Chris Cannon <Chris.c.cannon@gmail.com>
2018-10-19 15:30:24 -0700

    Create methods to handle account actions

--- atm.cpp
+++ atm.cpp
@@ -16,6 +16,29 @@ string founder_transactions;
 string acc1_transactions;
 string acc2_transactions;
 
+void balance_query()
+{
+  cout<<"The account balance is: ";
+  pthread_mutex_lock(&balance_mutex);
+  cout<<account_balance;
+  pthread_mutex_unlock(&balance_mutex);
+  cout<<endl;
+}
+
+void deposit(int deposit_amount)
+{
+  pthread_mutex_lock(&balance_mutex);
+  account_balance = account_balance + deposit_amount;
+  pthread_mutex_unlock(&balance_mutex);
+}
+
+void withdrawal(int withdrawal_amount)
+{
+  pthread_mutex_lock(&balance_mutex);
+  account_balance = account_balance - withdrawal_amount;
+  pthread_mutex_unlock(&balance_mutex);
+}
+
 void *thread_founder(void *arg)
 {
   
@@ -34,6 +57,8 @@ void *thread_acc2(void *arg)
 int main(int argc, char *argv[])
 {
   account_balance = 0;
+
+  thread_ids = new pthread_t[3];
 
   ifstream inputFile(argv[1]);
   num_founder_transactions = "";
@@ -54,5 +79,9 @@ int main(int argc, char *argv[])
     getline(inputFile, throwaway_string);
     getline(inputFile, num_acc2_transactions);
     getline(inputFile, acc2_transactions);
+
+    pthread_create(&thread_ids[0], NULL, thread_founder, (void *) args);
+    pthread_create(&thread_ids[1], NULL, thread_acc1, (void *) args);
+    pthread_create(&thread_ids[2], NULL, thread_acc2, (void *) args);
   }
 }
